f0a951c3fa01c372864d5951adecf17498568545
Houcine-code <houc.bh90@gmail.com>
2021-11-26 23:26:17 +0100

    Updates

---
+++ precache-manifest.0751721e5d6cfc157aad92124ca77d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5bf5cc84d87f76c434b9fb47d72e88",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "e089f781ac50fe629711",
+    "url": "/todo-list/static/css/main.2a9757b4.chunk.css"
+  },
+  {
+    "revision": "9263e911002048d6f9f7",
+    "url": "/todo-list/static/js/2.e14ebd43.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-list/static/js/2.e14ebd43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e089f781ac50fe629711",
+    "url": "/todo-list/static/js/main.61a310e3.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
